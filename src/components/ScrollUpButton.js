bab0a00b23cef0370710794599a85673f40e5005
gurudeep88 <gurudeep88@gmail.com>
2024-02-10 18:28:19 +0530

    sec commit  3

--- src/components/ScrollUpButton.js
+++ src/components/ScrollUpButton.js
@@ -1,55 +1,18 @@
 import React from 'react';
 
-class ScrollUpButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            scrollC: 0
-        };
-
-        this.handleScrolltoTop = this.handleScrolltoTop.bind(this);
-        this.onScroll = this.onScroll.bind(this);
-    }
-
-    componentDidMount() {
-        window.addEventListener("scroll", this.onScroll);
-    }
-
-    onScroll() {
-        this.setState({
-            scrollC: window.scrollY
-        });
-    }
-    
-    handleScrolltoTop() {
-        if(window !== undefined) {
-            window.scrollTo({top: 0, behavior: (this.props.behavior?this.props.behavior:'auto')})
-        }
-    }
-
-    render() {
-
-        const { 
-            IconSize,
-            appearCoordinate,
-        } = this.props;
-
+const ScrollUpButton =({text, onClick}) =>{
         return(
             <React.Fragment>
-
                 <div>
-
                 <p>Scroll Up</p>
                     <button 
+                    onClick={onClick}
                     >
-                        Icon
+                        {text}
                     </button>
-
                 </div>
-
             </React.Fragment>
         )
-    }
 }
 
 export default ScrollUpButton;
