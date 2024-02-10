567d23f365547be616abe105511da2f710c744d9
gurudeep88 <gurudeep88@gmail.com>
2024-02-10 18:53:34 +0530

    sec commit  312

--- src/components/ScrollUpButton.js
+++ src/components/ScrollUpButton.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const ScrollUpButton =({text, onClick}) =>{
+
         return(
             <React.Fragment>
                 <div>
                 <p>Scroll Up</p>
                     <button 
                     onClick={onClick}
+                    style={{color: text ===' updates' ? 'yellow': 'red'}}
                     >
                         {text}
                     </button>
