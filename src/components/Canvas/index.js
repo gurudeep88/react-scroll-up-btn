daaa532add59eda3ce5f229da0796fa43580b5d2
gurudeep88 <gurudeep88@gmail.com>
2024-02-10 21:31:31 +0530

    babel updated

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,44 +1,44 @@
-import React from 'react'
-import { useOnDraw } from '../../hooks/useOnDraw'
+// import React from 'react'
+// import { useOnDraw } from '../../hooks/useOnDraw'
 
-const Canvas = ({width, height}) => {
-    const {setCanvasRef, onMouseDown} = useOnDraw(onDraw);
+// const Canvas = ({width, height}) => {
+//     const {setCanvasRef, onMouseDown} = useOnDraw(onDraw);
 
-    function onDraw (data) {
-      console.log('data s', data)
-      drawLine(data.ctx, data.point, data.prevPoint, '#000', 5);
-    }
+//     function onDraw (data) {
+//       console.log('data s', data)
+//       drawLine(data.ctx, data.point, data.prevPoint, '#000', 5);
+//     }
 
-    function drawLine(ctx, end, start, color, width) {
-      start = start ?? end;
-      ctx.beginPath();
-      ctx.lineWidth= width;
-      ctx.strokeStyle = color;
-      ctx.moveTo(start.x, start.y);
-      ctx.lineTo(end.x, end.y);
-      ctx.stroke();
+//     function drawLine(ctx, end, start, color, width) {
+//       start = start ?? end;
+//       ctx.beginPath();
+//       ctx.lineWidth= width;
+//       ctx.strokeStyle = color;
+//       ctx.moveTo(start.x, start.y);
+//       ctx.lineTo(end.x, end.y);
+//       ctx.stroke();
 
-      ctx.fillStyle = color;
-      ctx.beginPath();
-      ctx.arc(start.x, start.y, 2, 0, 2 * Math.PI);
-      ctx.fill()
-    }
+//       ctx.fillStyle = color;
+//       ctx.beginPath();
+//       ctx.arc(start.x, start.y, 2, 0, 2 * Math.PI);
+//       ctx.fill()
+//     }
 
-  return (
-    <div>
-        <canvas 
-            width={width}
-            height={height}
-            style={canvasStyle}
-            ref={setCanvasRef}
-            onMouseDown={onMouseDown}
-        />
-    </div>
-  )
-}
+//   return (
+//     <div>
+//         <canvas 
+//             width={width}
+//             height={height}
+//             style={canvasStyle}
+//             ref={setCanvasRef}
+//             onMouseDown={onMouseDown}
+//         />
+//     </div>
+//   )
+// }
 
-export default Canvas
+// export default Canvas
 
-const canvasStyle={
-    border: `1px solid black`,
-}
+// const canvasStyle={
+//     border: `1px solid black`,
+// }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import ScrollUpButton from "./components/ScrollUpButton";
 import MyComponent from "./components/MyComponent";
-import Canvas from "./components/Canvas";
+//import Canvas from "./components/Canvas";
 
 export {
     ScrollUpButton,
     MyComponent,
-    Canvas
+   // Canvas
 }
