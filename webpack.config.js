78c9c654640fd3a1fccbcb7ae2208df8cb826c22
gurudeep88 <gurudeep88@gmail.com>
2024-02-10 17:47:42 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/ScrollUpButton.js',
+  output: {
+    path: path.resolve('lib'),
+    filename: 'ScrollUpButton.js',
+    library: "react-scroll-up-button",      
+    libraryTarget: 'umd',      
+    publicPath: '/lib/',      
+    umdNamedDefine: true 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom      
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  }
+};
